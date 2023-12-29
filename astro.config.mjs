f55c09bbec1d23b9352f38906e11277dfae75448
Gerardo Marx Chávez-Campos <gmarx_cc@itmorelia.edu.mx>
2023-12-29 09:17:01 -0600

    site info updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://blog-gmarx.itmorelia.com',
   integrations: [sitemap(), react(), image(), tailwind()]
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Gerardo Marx Blog'
+export const SITE_DESCRIPTION = 'A Blog for Scince, Technology, and Programming'
+export const HOMEPAGE_URL = 'https://www.craftz.dog/'
