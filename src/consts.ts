44d15c50ff610914ad33d83246262c85747433ba
Gerardo Marx Chávez-Campos <gmarx_cc@itmorelia.edu.mx>
2024-01-03 17:31:24 -0600

    masthead

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 export const SITE_TITLE = 'Gerardo Marx Blog'
 export const SITE_DESCRIPTION = 'A Blog for Scince, Technology, and Programming'
-export const HOMEPAGE_URL = 'https://www.craftz.dog/'
+export const HOMEPAGE_URL = 'https://gmarx.itmorelia.com'
